2709aaf20bdfe6e921113c2534655618206d8d29
MarcosPaul0 <marcosphip7@gmail.com>
2022-11-04 19:36:19 -0300

    feat: adiciona rota de confirmação de email

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, UseGuards, Req, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Req,
+  HttpStatus,
+  Get,
+  Param,
+} from '@nestjs/common';
 import { NestResponseBuilder } from '../core/http/nestResponseBuilder';
 import { NestResponse } from '../core/http/nestResponse';
 import { AuthService } from './auth.service';
@@ -29,6 +37,22 @@ export class AuthController {
     const response = new NestResponseBuilder()
       .setStatus(HttpStatus.OK)
       .setBody(tokens)
+      .build();
+
+    return response;
+  }
+
+  @Get('confirm/:token')
+  async receivedConfirmationAccountMail(
+    @Param('token') token: string,
+  ): Promise<NestResponse> {
+    const activeUserResponse =
+      await this.authService.receivedConfirmationAccountMail(token);
+
+    const response = new NestResponseBuilder()
+      .setStatus(HttpStatus.OK)
+      .setHeaders({ Location: `/users/${activeUserResponse.user.id}` })
+      .setBody(activeUserResponse)
       .build();
 
     return response;
